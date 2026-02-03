4bbe35ed7602d400d1d1b3730368d4db2c661f55
Kad <114583468+Kad-Caiel@users.noreply.github.com>
2026-02-03 01:20:20 +0100

    Add files via upload

---
+++ [Profession_Assistant - esES only] localization.lua
@@ -0,0 +1,88 @@
+if (locale == 'esES') then
+	-- Options Panel --
+	L["Main"] = "Principal"
+	L["Options"] = "Opciones"
+	L["Professions"] = "Profesiones"
+	L["Global"] = "Global"
+	-- buttons --
+	L["Enable chat"] = "Mostrar en el chat"
+	L["Shown when"] = "Cuándo mostrar"
+	L["On Shift"] = "Con Shift"
+	
+	L["Enable Master"] = "Interruptor maestro"
+	L["Master switch"] = "Habilitar interruptor maestro"
+	
+	L["Show Icons"] = "Mostrar iconos"
+	L["Show icons in front of result items"] = "Mostrar iconos delante de los objetos resultantes"
+	
+	L["Minimap Button"] = "Botón del minimapa"
+	L["Enable Minimap"] = "Habilitar botón del minimapa"
+	L["Toggle minimap button"] = "Mostrar botón del minimapa"
+	
+	L["Herbalism Locations"] = "Localizaciones de Herboristería"
+	L["Enable Herbalism"] = "Activar Herboristería"
+	L["Toggle herbalism locations on herbs"] = "Mostrar localizaciones de herboristería en las hierbas"
+	
+	L["Toggle chat herbalism locations on herbs"] = "Mostrar localizaciones de herboristería en al chat"
+	L["Hold Shift to show Herbalism locations"] = "Mantén Shift para mostrar localizaciones de herboristería"
+	
+	L["Milling"] = "Molienda"
+	L["Enable Milling"] = "Activar molienda"
+	L["Toggle milling information on herbs"] = "Mostrar información de molienda en las hierbas"
+	L["Toggle chat information on herbs"] = "Mostrar información de molienda en el chat"
+	L["Hold Shift to show Milling information"] = "Mantén Shift para mostrar información de la molienda"
+	
+	L["Enable Herb"] = "Habilitar en hierbas"
+	L["Toggle herb information on pigments"] = "Mostrar información sobre las hierbas en los pigmentos"
+	L["Toggle chat information on pigments"] = "Mostrar información de los pigmentos en el chat"
+	L["Hold Shift to show Herb information"] = "Mantén Shift para mostrar la información de hierbas"
+	
+	L["Mining Locations"] = "Localizaciones de Minería"
+	L["Enable Mining"] = "Habilitar Minería"
+	L["Toggle mining locations on ores"] = "Mostrar localizaciones de minería en las menas"
+	L["Toggle chat mining locations on ores"] = "Mostrar localizaciones de minería en el chat"
+	L["Hold Shift to show Mining locations"] = "Mantén Shift para mostrar localizaciones de minería"
+	
+	L["Prospecting"] = "Prospección"
+	L["Enable Prospecting"] = "Habilitar Prospección"
+	L["Toggle prospecting information on ores"] = "Mostrar información de prospección en las menas"
+	L["Toggle chat information on ores"] = "Mostrar información de prospección en el chat"
+	L["Hold Shift to show Prospecting information"] = "Mantén Shift para mostrar la información de prospección"
+	
+	L["Enable Ore"] = "Habilitar Menas"
+	L["Toggle ore information on gems"] = "Mostrar información de menas en las gemas"
+	L["Toggle chat information on gems"] = "Mostrar información de gemas en el chat"
+	L["Hold Shift to show Ore information"] = "Mantén Shift para mostrar información de menas"
+	
+	L["Enchanting"] = "Encantamiento"
+	L["Enable Enchanting"] = "Habilitar Encantamiento"
+	L["Toggle Disenchanting information on gear"] = "Mostrar información de desencantamiento en el equipo"
+	L["Toggle chat information on gear"] = "Mostrar información de desencantamiento en el chat"
+	L["Hold Shift to show Disenchanting information"] = "Mantén Shift para mostrar información de desencantamiento"
+	
+	L["Enable Materials"] = "Habilitar materiales"
+	L["Toggle gear source information on enchanting materials"] = "Mostrar equipo de origen en los materiales de encantamiento"
+	L["Toggle chat information on enchanting materials"] = "Mostrar información de materiales de encantamiento en el chat"
+	L["Hold Shift to show Material information"] = "Mantén Shift para mostrar información de materiales"
+	-- Tooltip Info --
+	L["Found in"] = "Encontrado en"
+	
+	L["Milling outcome"] = "Resultados de la molienda"
+	L["Milled from"] = "Molido de"
+	
+	L["Prospecting outcome"] = "Resultados de la prospección"
+	L["Prospected from"] = "Prospectado de"
+	
+	L["Disenchant into"] = "Se desencanta en"
+	L["Disenchant from"] = "Desencantado de"
+	
+	L["Nodes"] = "Nodos"
+	-- Custom translations required --
+	L["Frozen Herb"] = "Hierba congelada"
+	L["Netherdust Bush"] = "Arbusto de polvo abisal"
+	L["Firethorn"] = "Espino de fuego"
+	L["Sha-Touched Herb"] = "Hierba influenciada por el Sha"
+	L["Withered Herb"] = "Hierba marchita"
+	L["Fel-Encrusted Herb"] = "Hierba con costra vil"
+	L["World Quest"] = "Misión de mundo"
+end
